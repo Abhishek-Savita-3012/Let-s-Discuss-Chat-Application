f9119836e916ece02b619f764635bcb7745b4bda
Abhishek-Savita-3012 <asavita3012@gmail.com>
2025-07-23 16:29:01 +0530

    Create client.js

---
+++ js/client.js
@@ -0,0 +1,50 @@
+//This site is different than nodeServer, we are connecting this site to that site
+const socket = io('http://localhost:8000', {transports:['websocket']});
+
+//Get DOM elements in respective JS Variables
+const form = document.getElementById("send-container");
+const messageInput = document.getElementById("messageInp");
+const messageContainer = document.querySelector(".container");
+
+//Audio that will play on receiving messages
+var audio = new Audio('ting.mp3')
+
+//Function which will append event info to the container
+const append = (message, position)=>{
+    const messageElement = document.createElement('div');
+    messageElement.innerText = message;
+    messageElement.classList.add('message');
+    messageElement.classList.add(position);
+    messageContainer.append(messageElement);
+    if(position == 'left'){
+        audio.play();
+    }
+}
+
+//Ask new user for his/her name and let the server know
+const myname = prompt("Enter your name to join");
+socket.emit('new-user-joined', myname);
+ 
+//If a new user jouns, receive his/her name from the server
+socket.on('user-joined', myname=>{
+    append(`${myname} joined the chat`, 'right');
+});
+
+//If server sends a message, receive it
+socket.on('receive', data=>{
+    append(`${data.myname} : ${data.message}`, 'left');
+});
+
+//If a user leaves the chat, append the info to the container
+socket.on('leave', myname =>{
+    append(`${myname} left the chat`, `right`);
+});
+
+//If the form gets submitted, send server the message
+form.addEventListener('submit', (e)=>{
+    e.preventDefault();    //helps in preventing reload of page
+    const message = messageInput.value;
+    append(`You: ${message}`, 'right');
+    socket.emit('send', message);
+    messageInput.value = '';
+})
